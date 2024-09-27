334063228ddf9030645f8aaf38733d1d70df02c5
Digital2512 <valerieannabella123@gmail.com>
2024-09-27 16:00:50 +0700

    Implement intial file and folder structure
    Using Next JS and routing technique, Intial Home, Register and Login Pages

---
+++ finance-management-application/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import {Inter, IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  variable: "--font-ibm-plex-serif",
+  weight: ["100", "200", "300", "400", "500", "600", "700"]
+});
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "WalletWiz",
+  description: "Your Magic Tool for Smart Spending!",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
